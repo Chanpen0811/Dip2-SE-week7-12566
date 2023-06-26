e488428d1de72ad94b5a1b37a7bb5108b0c27cfc
chanpen <jan1574p@gmail.com>
2023-06-26 16:02:49 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  Widget MyApp =MaterialApp(
+    home: Scaffold(
+     // backgroundColor: Colors.purple,
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text("Home"),
+        leading: Icon(Icons.arrow_back),
+      ),
+      body: Text("Myapp"),
+    ),
+  );
+
+  runApp(MyApp);
+}
